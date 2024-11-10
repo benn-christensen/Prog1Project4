a90ed1118af713547ba0d61f147cbd02c5879d62
Benn Christensen <benn.christensen@wolterskluwer.com>
2024-11-10 10:07:28 +0100

    changed toString method for ListView

--- src/application/models/Customer.java
+++ src/application/models/Customer.java
@@ -26,6 +26,6 @@ public class Customer {
 
     @Override
     public String toString() {
-        return "Kunde{" + "navn = " + name + ", mobilnummer = " + mobileNumber + '}';
+        return name;
     }
 }
